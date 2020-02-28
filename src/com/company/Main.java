7f30fe3255e3af62ecb10008ffd1311f60e06684
summit.smith <summitsmith842@gmail.com>
2020-02-28 16:35:26 +0300

    my first calculation with OOP

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// a program that calculates the area of a rectangle
+        int a=22;
+        int b=44;
+        System.out.println("the area of a rectangle is " + a * b);
+    }
+}
